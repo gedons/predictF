c0aecc1390d9121bac04cf124641df516d4206fb
Nerdy Gedoni <gedoniani@gmail.com>
2025-11-21 11:43:39 +0100

    Endpoint Update

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useAuthStore } from '../store/auth'
 
 //const API_BASE =  'http://127.0.0.1:8000'
-const API_BASE =  process.env.VITE_API_BASE || 'http://127.0.0.1:8000'
+const API_BASE =  process.env.VITE_API_BASE
 
 const api = axios.create({
   baseURL: API_BASE,
